8f487cb8cdc88c35f9625e55d4bcd977c8560cf1
Kaung Zaw Htet <kaungzawhtet.mm@gmail.com>
2025-03-02 23:31:16 +0700

    feat: InvalidLastChar updated

--- App/Constants/Messages.cs
+++ App/Constants/Messages.cs
@@ -4,7 +4,7 @@ public static class Messages
 {
     public const string InvalidCharGroupErrorMessage = "This is invalid character group.";
     public const string KeyNotFoundErrorMessage = "Key Not Found.";
-    public const string InvalidLastChar = "Last Char should be #";
+    public const string InvalidLastChar = "Last Char should be a send #";
 
     public const string InvalidChars = "Only number, space, * and # are allowed.";
 
